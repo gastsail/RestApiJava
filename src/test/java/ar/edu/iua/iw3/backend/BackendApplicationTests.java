d74f66d966e35fd03b9a335b9408b17d546a3353
Gaston Saillen <gsaillen95@gmail.com>
2020-08-17 12:34:50 -0300

    Base commit

---
+++ src/test/java/ar/edu/iua/iw3/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.iua.iw3.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
